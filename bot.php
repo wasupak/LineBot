70864db60a2de63f22e066d16a3ea36888cc3bb4
wasupak <37166322+wasupak@users.noreply.github.com>
2018-03-09 11:45:44 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -15,10 +15,15 @@ if (!is_null($events['events'])) {
     // Get replyToken			
     $replyToken = $event['replyToken'];			
     // Build message to reply back			
-    $messages = [				'type' => 'text',				'text' => $text			];			
+    if($text=='keyword') {
+      $messages = ['type' => 'text','text' => "ตอบตาม keyword"];			
+    } else {
+      $messages = ['type' => 'text','text' => $text];			
+    }
+    
     // Make a POST Request to Messaging API to reply to sender			
     $url = 'https://api.line.me/v2/bot/message/reply';			
-    $data = [				'replyToken' => $replyToken,				'messages' => [$messages],			];			
+    $data = ['replyToken' => $replyToken,'messages' => [$messages],];			
     $post = json_encode($data);			
     $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);			
     $ch = curl_init($url);			
